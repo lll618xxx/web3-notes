3c8c499467973e969683f35535638d04482d9931
nxiang <837435186@qq.com>
2023-02-07 09:33:51 +0800

    fix：修复已知问题

---
+++ web3Simple/client/src/utils/constants.ts
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress:string = "0xb4e7ee11153fFE79b779D75a88EaE42D7b3b737C";
+export const contractABI:array = abi.abi;
--- web3Simple/smart_contract/hardhat.config.js
+++ web3Simple/smart_contract/hardhat.config.js
@@ -1,11 +1,7 @@
 require('@nomiclabs/hardhat-waffle');
+const networks = require('./config/config')
 
 module.exports = {
   solidity: '0.8.0',
-  networks: {
-    goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/q-jWxrQ_HoAeMPl_9gkO3KMZzlmt4_qx',
-      accounts: ['2cecca4d1ef1f04856d79b458ee87e6c2994f1eb78f738c94d0bfccd627865af'],
-    },
-  },
+  networks
 };
