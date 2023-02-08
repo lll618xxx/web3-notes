bf2ef5790c2b99473b11217c68c5a413bf138beb
nxiang <837435186@qq.com>
2023-02-08 09:56:26 +0800

    feat：adjust compileLocalSolidity

--- compileLocalSolidity/deploy.js
+++ compileLocalSolidity/deploy.js
@@ -2,22 +2,26 @@ const ethers = require('ethers')
 const fs = require('fs-extra')
 
 async function main() {
-    const rpcServer = '' // Ganache
-    const provider = new ethers.providers.JsonRpcProvider(rpcServer) // 连接本地区块链
+    try {
+        const rpcServer = 'http://127.0.0.1:7545' // Ganache
+        const provider = new ethers.providers.JsonRpcProvider(rpcServer) // 连接本地区块链
+       
+        const privateKey = '9697527aca44d93fc75dbe0ceac67740f2f50b0a66b14fd6659483b9fc848e4f'
+        const wallet = new ethers.Wallet(privateKey, provider)
 
-    const privateKey = ''
-    const wallet = new ethers.Wallet(privateKey, provider)
+        const abi = fs.readFileSync('./Local_sol_Local.abi', 'utf8')
+        const binary = fs.readFileSync('./Local_sol_Local.bin', 'utf8')
 
-    const abi = fs.readFileSync('./Local_sol_Local.abi', 'utf8')
-    const binary = fs.readFileSync('./Local_sol_Local.bin', 'utf8')
-
-    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
-    console.log('deploying...')
-    const contract = await contractFactory.deploy()
-    console.log(contract)
+        const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+        console.log('deploying...')
+        const contract = await contractFactory.deploy()
+        console.log('合约地址', contract.address)
+    } catch (error) {
+        console.log({error})
+    }
 }
 
-main.then(() => {
+main().then(() => {
     process.exit(0)
 }).catch((error) => {
     process.exit(1)
